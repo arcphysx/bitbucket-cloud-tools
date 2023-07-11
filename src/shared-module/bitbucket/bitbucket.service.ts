25abb300315b4fa190cf5dd9cd556bd7f60f093a
mo.purnomo <mo.purnomo@gdn-commerce.com>
2023-07-11 14:32:29 +0700

    fix method

--- src/shared-module/bitbucket/bitbucket.service.ts
+++ src/shared-module/bitbucket/bitbucket.service.ts
@@ -5,9 +5,9 @@ import { Bitbucket } from 'bitbucket'
 @Injectable()
 export class BitbucketService {
 
-    async getPullRequestListByWorkspaceAndRepoSlug(credential: BitbucketCredential, workspace: string, repo_slug: string){
+    async getPullRequestListByWorkspaceAndRepoSlug(credential: BitbucketCredential, workspace: string, repo_slug: string, page: number = 1, pagelen: number = 10){
         const bitbucketSdk = new Bitbucket({auth: credential})
-        let prList = await bitbucketSdk.pullrequests.list({workspace: workspace, repo_slug})
+        let prList = await bitbucketSdk.pullrequests.list({workspace: workspace, repo_slug: repo_slug, page: page.toString(), pagelen: pagelen})
         return prList
     }
 }
